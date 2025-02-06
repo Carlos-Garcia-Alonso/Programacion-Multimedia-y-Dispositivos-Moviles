a0c26a69d0dc92d9df75b860c41c446ee41895cf
Carlos <carlos.garcia@ciclosmontecastelo.com>
2025-02-06 10:39:03 +0100

    faltaba poner el repositorio como private en el getUseCase de ChuckNorris

--- app/src/main/java/com/carlosgarciaalonso/wrestlingapp/domain/GetChuckNorrisUseCase.kt
+++ app/src/main/java/com/carlosgarciaalonso/wrestlingapp/domain/GetChuckNorrisUseCase.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class GetChuckNorrisUseCase @Inject constructor(val repository: ChuckRepository) {
+class GetChuckNorrisUseCase @Inject constructor(private val repository: ChuckRepository) {
 
     suspend operator fun invoke() : ChuckNorrisJoke {
 
